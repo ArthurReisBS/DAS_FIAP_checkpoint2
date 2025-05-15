1a8ca1358ed49cc59b86f14f11b771a24454dc1f
Manoela Oliveira <manoela.oliveira0900@gmail.com>
2025-05-15 16:03:38 -0300

    Exercicio Fatoriais

--- Checkpoint2/main.c
+++ Checkpoint2/main.c
@@ -5,11 +5,12 @@
 int main(){
     setlocale (LC_ALL, "Portuguese_Brazil");
     int exercicios;
+    int n_fatorial;
 
     do{
-    printf(" -----------------------\n");
-    printf(" MENU DE EXERCÍCIOS\n");
-    printf(" -----------------------\n");
+    printf(" ----------------------\n");
+    printf("   MENU DE EXERCÍCIOS\n");
+    printf(" ----------------------\n");
     printf("1- Sequência de Fibonacci\n");
     printf("2- Fatoriais\n");
     printf("3- Verificar Palíndromo\n");
@@ -24,6 +25,25 @@ int main(){
                 break;
             case 2:
                 printf("\nVocê selecionou a opção 2.\n");
+                printf("Digite um número de 1 a 20: ");
+                scanf("%d", &n_fatorial);
+
+                if (n_fatorial < 1 || n_fatorial > 20) {
+                        printf("O número deve estar entre 1 e 20 e ser um número inteiro.\n");
+                return;
+                }
+
+                unsigned long long vetor[20];
+                vetor[0] = 1;
+                for (int i = 1; i < n_fatorial; i++) {
+                        vetor[i] = vetor[i - 1] * (i + 1);
+                }
+
+                printf("\nFatoriais:\n");
+                for (int i = 0; i < n_fatorial; i++) {
+                        printf("%d! = %llu\n", i + 1, vetor[i]);
+                }
+
                 break;
             case 3:
                 printf("\nVocê selecionou a opção 3.\n");
